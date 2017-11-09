e1d4eb3e7a73343e399a5f9f516421f2767a67ba
Donnie Flood <donaldflood@gmail.com>
2017-11-09 11:22:34 -0700

    moved from cfmr project to own project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cfmr',
+  packages = ['cfmr'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Python support library for CFMR',
+  author = 'Donnie Flood',
+  author_email = 'pypi@floodfx.com',
+  url = 'https://github.com/floodfx/cfmr',
+  download_url = 'https://github.com/floodfx/cfmr/archive/0.0.1.tar.gz',
+  keywords = ['map', 'reduce', 'cloud', 'function', 'aws', 'lambda', 'azure', 'google', 'cloud'], 
+  classifiers = [],
+)
