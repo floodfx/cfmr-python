bae9763b13ada72325f2a95521024ddb142947b4
Donnie Flood <donaldflood@gmail.com>
2017-11-09 11:24:18 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Python support library for CFMR',
   author = 'Donnie Flood',
   author_email = 'pypi@floodfx.com',
-  url = 'https://github.com/floodfx/cfmr',
-  download_url = 'https://github.com/floodfx/cfmr/archive/0.0.1.tar.gz',
+  url = 'https://github.com/floodfx/cfmr-python',
+  download_url = 'https://github.com/floodfx/cfmr-python/archive/0.0.1.tar.gz',
   keywords = ['map', 'reduce', 'cloud', 'function', 'aws', 'lambda', 'azure', 'google', 'cloud'], 
   classifiers = [],
 )
