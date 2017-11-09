0d53640f0c88a3090b18a97f30a8982e808e7be9
Donnie Flood <donaldflood@gmail.com>
2017-11-09 11:50:48 -0700

    moved module out of folder into root dir

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'cfmr',
-  packages = ['cfmr'], # this must be the same as the name above
-  version = '0.0.2',
+  packages = ['cfmr'], 
+  version = '0.0.3',
   description = 'Python support library for CFMR',
   author = 'Donnie Flood',
   author_email = 'pypi@floodfx.com',
   url = 'https://github.com/floodfx/cfmr-python',
-  download_url = 'https://github.com/floodfx/cfmr-python/archive/0.0.2.tar.gz',
+  download_url = 'https://github.com/floodfx/cfmr-python/archive/0.0.3.tar.gz',
+  install_requires=['boto3'],
   keywords = ['map', 'reduce', 'cloud', 'function', 'aws', 'lambda', 'azure', 'google', 'cloud'], 
   classifiers = [],
 )
